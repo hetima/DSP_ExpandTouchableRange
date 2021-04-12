7175cf54bf566b5510501dbe5ef154cb03cd1bc0
hetima <hetima@hetima.com>
2021-04-13 02:56:04 +0900

    v1.0

---
+++ ExpandTouchableRange/ExpandTouchableRange.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace ExpandTouchableRange
+{
+    [BepInPlugin(__GUID__, __NAME__, "1.0.0")]
+    public class ExpandTouchableRange : BaseUnityPlugin
+    {
+        public const string __NAME__ = "ExpandTouchableRange";
+        public const string __GUID__ = "com.hetima.dsp." + __NAME__;
+
+        public static ConfigEntry<int> touchableRangeMultiplier;
+        internal static float multiply;
+
+        void Awake()
+        {
+            touchableRangeMultiplier = Config.Bind<int>("General", "touchable_range_multiplier", 250, "multiplier percentage. min=20 max=500. original behavior is 100");
+            touchableRangeMultiplier.Value = Math.Min(Math.Max(touchableRangeMultiplier.Value, 20), 500);
+            multiply = (float)touchableRangeMultiplier.Value / 100f;
+
+            new Harmony(__GUID__).PatchAll(typeof(GetObjectSelectDistance_Patch));
+        }
+
+
+
+        [HarmonyPatch(typeof(PlayerAction_Inspect), "GetObjectSelectDistance")]
+        public static class GetObjectSelectDistance_Patch
+        {
+            public static void Postfix(ref float __result)
+            {
+                __result = __result * multiply;
+            }
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# ExpandTouchableRange 
+
+Mod for Dyson Sphere Program.
+
+Expand interactable range to open storage and other building info by click.  
+
+ストレージを開いたり建物の情報を見たりできる距離を広げます。デフォルトで2.5倍になります。設定ファイルで変更できます。  
+
+
+## Config
+
+config file name is `com.hetima.dsp.ExpandTouchableRange.cfg`.
+
+### `touchable_range_multiplier`  
+
+multiplier percentage. min=20(%) max=500(%). original behavior is 100. default value is 250.
+
+## Release Notes
+
+### v1.0.0
+
+- Initial Release
+
